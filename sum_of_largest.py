f61ecd6e4f6978270bde44dd2ff72819ab38a9ba
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-12 22:06:32 -0400

    Implemented function in simple terms

--- sum_of_largest.py
+++ sum_of_largest.py
@@ -1,7 +1,27 @@
 def sumLargestNumbers(data):
-  # Put your solution here
+  largest = 0
+  second_largest = 0
+
+  # Finding largest
+  for i in data:
+    if i > largest:
+      largest = i
+  
+  # Finding second largest
+  for j in data:
+    if largest > j > second_largest:
+      second_largest = j
+  
+  return largest + second_largest
 
 
 print(sumLargestNumbers([1, 10]))
 print(sumLargestNumbers([1, 2, 3]))
 print(sumLargestNumbers([10, 4, 34, 6, 92, 2]))
+
+
+
+# First interate through the list of numbers
+# Find the max value of the list and save it
+# Remove the max value from the list
+# Repeat steps 1 and 2
