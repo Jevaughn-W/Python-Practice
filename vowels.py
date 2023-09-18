ce8b2dc304a7498cdcb1e246f2d7d51366c0a3c5
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-18 08:12:29 -0400

    Added in the vowels exercise and coverted JS to python

---
+++ vowels.py
@@ -0,0 +1,10 @@
+# Count the number if vowels that appear in a given string
+
+
+def numberOfVowels(data): 
+  #Put your solution here
+  return None
+
+print(numberOfVowels("orange"))
+print(numberOfVowels("lighthouse labs"))
+print(numberOfVowels("aeiou"))
