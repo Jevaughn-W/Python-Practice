5818f3ae8ff8da3de76a0e62ec09d21416e2cf73
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-18 08:20:56 -0400

    Completed exercise by leveraging pythons built in in function

--- vowels.py
+++ vowels.py
@@ -2,9 +2,23 @@
 
 
 def numberOfVowels(data): 
-  #Put your solution here
-  return None
+  vowels = ["a","e","i","o","u"]
+  result = 0
+  for letter in data:
+    if letter in vowels: # By using key word in we can check is something is contained in an array vs using array.include
+      result += 1
+  
+  return result
 
 print(numberOfVowels("orange"))
 print(numberOfVowels("lighthouse labs"))
 print(numberOfVowels("aeiou"))
+
+# Create an array of vowels
+# Create a result variable and set it equal to 0
+# Loop through the strong provided
+# Do a if statement which checks if the vowel array contains the current iteration of the loop
+# If true add one to the result
+# End loop and return result
+
+
