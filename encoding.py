a8119a677b60f10e012b9c31c96a68cb284364c2
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-19 08:27:39 -0400

    Create exercise and add steps for solving

---
+++ encoding.py
@@ -0,0 +1,19 @@
+# Given a normal string of words, replace all the white space with %20
+
+
+def urlEncode(text):
+  # for char in text:
+  #   print(char)
+  return None
+
+
+print(urlEncode("Lighthouse Labs"))
+print(urlEncode(" Lighthouse Labs "))
+print(urlEncode("blue is greener than purple for sure"))
+
+
+# Create a results array
+# Loop through the string using for in
+# Trim the outer spaces of the array
+# Check in the value is a space - if it is a space append %20 to the result array otherwise append the letter
+# At the end of the for loop collapse the array and return the result
