dede59c0552e24a3850b154693faabf86bdfb1ae
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-23 16:02:42 -0400

    Completed exercise, required combination of list and dictionary to solve

--- smart_parking.py
+++ smart_parking.py
@@ -1,8 +1,29 @@
 # returns the coordinates of an available parking spot for the vehicle, or returns false if there is no available spot
 
 def whereCanIPark(spots, vehicle):
-  #Code here!
-  return None
+  # Loop outer array
+  # Loop inner array
+  # Check if letter is capitalized
+  # If yes check if 
+
+  x_coordinate = 0
+  y_coordinate = 0
+
+  keys = { # Created a dictionary that contains the vehicles allowed in each parking
+    "S" : ["small", "motorcycle"],
+    "R" : ["small", "regular", "motorcycle"],
+    "M": ["motorcycle"] 
+  }
+
+  for spot in spots:
+    for space in spot:
+      if space.isupper() and vehicle in keys[space]: #identifying the letters are capitalized and checking if the vehicle is within the letter
+        return [x_coordinate ,y_coordinate] # Returns the array
+      x_coordinate += 1 # Adds 1 to the x-coordinate if there isn't anything found
+    x_coordinate = 0 # Need to reset the x-coordinate to zero for each new row
+    y_coordinate += 1 # Adds 1 to the y-cordinate if there isn't anything found in the entire row
+
+  return False
 
 print(whereCanIPark(
   [
