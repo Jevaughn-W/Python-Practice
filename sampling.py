e0e8f3303ba588d5b1a56690c01cfac696fff94e
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-23 16:28:17 -0400

    Implemented solution with algorithim in solution

--- sampling.py
+++ sampling.py
@@ -1,8 +1,16 @@
 # Create a function that takes in 2 arguments, a list of strings and a decimal. The function assess the count of dirty and clean and check that the % of dirty is less than the threshold of the second argument
 
 def checkAir(samples, threshold):
-  #Code here!
-  return None
+  
+  counter = 0 # Variable to keep track of the count of dirty sample
+  
+  for sample in samples: # Looper
+    if sample == "dirty":
+      counter += 1 
+  if (counter / len(samples)) > threshold :
+    return "Polluted"
+  else:
+    return "Clean"
 
 print(checkAir(
   ['clean', 'clean', 'dirty', 'clean', 'dirty', 'clean', 'clean', 'dirty', 'clean', 'dirty'],
@@ -18,3 +26,9 @@ print(checkAir(
   ['clean', 'dirty', 'clean', 'dirty', 'clean', 'dirty', 'clean'],
   0.9
 ))
+
+# Create a counter variable = 0
+# Take in list and traverse using for loop
+# For each element, check if there are any diry, if yes add one to counter
+# At the end of the loop check if the count divided by the list length is greater than the threshold
+# If yes return polluted else return clean
