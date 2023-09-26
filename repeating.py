28759e809191cb33c62ecb31b046a8606a0bb193
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-26 08:42:02 -0400

    Create exercise and convert from JS

---
+++ repeating.py
@@ -0,0 +1,20 @@
+# For the input of list, the first element should be repeated by the number of the second element
+
+def repeatNumbers(data):
+  # Put your solution here
+  return None
+
+print(repeatNumbers([[1, 10]]))
+print(
+  repeatNumbers([
+    [1, 2],
+    [2, 3],
+  ])
+)
+print(
+  repeatNumbers([
+    [10, 4],
+    [34, 6],
+    [92, 2],
+  ])
+)
