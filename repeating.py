e9078a42b34272f70cd21c71a20ec38a563b69ab
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-27 08:32:11 -0400

    Implement solution for one array

--- repeating.py
+++ repeating.py
@@ -1,20 +1,29 @@
 # For the input of list, the first element should be repeated by the number of the second element
 
 def repeatNumbers(data):
-  # Put your solution here
-  return None
+  result = []
+  for array in data:
+    inner_result =[]
+    for i in range(array[1]):  # 
+      inner_result.append(str(array[0]))
+  result.append("".join(inner_result))
+  return result
+
 
 print(repeatNumbers([[1, 10]]))
-print(
-  repeatNumbers([
-    [1, 2],
-    [2, 3],
-  ])
-)
-print(
-  repeatNumbers([
-    [10, 4],
-    [34, 6],
-    [92, 2],
-  ])
-)
+# print(
+#   repeatNumbers([
+#     [1, 2],
+#     [2, 3],
+#   ])
+# )
+# print(
+#   repeatNumbers([
+#     [10, 4],
+#     [34, 6],
+#     [92, 2],
+#   ])
+# )
+
+# Create a loop to loop through the list of arrays
+# Create a second loop that prints the first array element the number of times for the 2n element
