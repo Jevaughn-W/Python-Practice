652639bc548326b420420ca73bf2929ac3997acb
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-09-27 08:41:00 -0400

    Finalize result

--- repeating.py
+++ repeating.py
@@ -2,28 +2,34 @@
 
 def repeatNumbers(data):
   result = []
-  for array in data:
+  
+  
+  for array in data:  # Loop through array of arrays
+    
+    # Replicating number within the inner array
     inner_result =[]
-    for i in range(array[1]):  # 
-      inner_result.append(str(array[0]))
-  result.append("".join(inner_result))
-  return result
+    for i in range(array[1]):  # For loop which tells the the function how many times to repeat the number
+      inner_result.append(str(array[0])) 
+    result.append("".join(inner_result)) # Joins the values to the outer loop result
+
+
+  return ", ".join(result)
 
 
 print(repeatNumbers([[1, 10]]))
-# print(
-#   repeatNumbers([
-#     [1, 2],
-#     [2, 3],
-#   ])
-# )
-# print(
-#   repeatNumbers([
-#     [10, 4],
-#     [34, 6],
-#     [92, 2],
-#   ])
-# )
+print(
+  repeatNumbers([
+    [1, 2],
+    [2, 3],
+  ])
+)
+print(
+  repeatNumbers([
+    [10, 4],
+    [34, 6],
+    [92, 2],
+  ])
+)
 
 # Create a loop to loop through the list of arrays
 # Create a second loop that prints the first array element the number of times for the 2n element
